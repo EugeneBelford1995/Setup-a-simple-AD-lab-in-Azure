142e36c4efa65f99006a3ded5706e9c460ea0e0e
Rich <114684848+EugeneBelford1995@users.noreply.github.com>
2023-12-21 20:48:40 -0500

    Create Config the DC.ps1

---
+++ Config the DC.ps1
@@ -0,0 +1,6 @@
+Install-WindowsFeature AD-Domain-Services
+
+#Run commands & PS1s on Azure VMs
+#Start-AzVM -ResourceGroupName "ADLab" -Name "DC1"
+#Set-Location "C:\Users\fdhsr\Google Drive\Documents\CompTIA studying\Lab Domain Projects\00 Ideas\Setup AD lab in Azure"
+#Invoke-AzVMRunCommand -VMName "DC1" -ResourceGroupName "ADLab" -CommandId "RunPowerShellScript" -ScriptPath ".\Config the DC.ps1"
